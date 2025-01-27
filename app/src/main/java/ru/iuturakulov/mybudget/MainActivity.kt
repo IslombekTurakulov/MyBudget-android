7f019adfc22b626c94fb030c5c47342b0f2827bc
iuturakulov <me@turakulov.ru>
2025-01-27 04:00:27 +0300

    MYBUDGET-9: Add network condition overlay feature

--- app/src/main/java/ru/iuturakulov/mybudget/MainActivity.kt
+++ app/src/main/java/ru/iuturakulov/mybudget/MainActivity.kt
@@ -1,31 +1,46 @@
 package ru.iuturakulov.mybudget
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.launch
 import ru.iuturakulov.mybudget.core.worker.ProjectSyncWorker
+import ru.iuturakulov.mybudget.databinding.ActivityMainBinding
 import java.util.concurrent.TimeUnit
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
+    private val connectivityManager by lazy { getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val workRequest = PeriodicWorkRequestBuilder<ProjectSyncWorker>(
             5, TimeUnit.MINUTES
         ).build()
+
+        observeNetworkChanges()
 
         WorkManager.getInstance(this).enqueueUniquePeriodicWork(
             "ProjectSync",
@@ -39,7 +54,7 @@ class MainActivity : AppCompatActivity() {
         navController = navHostFragment.navController
 
         // Настройка BottomNavigationView
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        val bottomNavigationView = binding.bottomNavigation
         NavigationUI.setupWithNavController(bottomNavigationView, navController)
 
         // Скрываем BottomNavigationView на SplashScreen и авторизации
@@ -57,5 +72,78 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    private fun observeNetworkChanges() {
+        lifecycleScope.launch {
+            connectivityManager.observeNetworkStatus().collect { isConnected ->
+                updateNetworkStatus(isConnected)
+            }
+        }
+    }
+
+    private fun updateNetworkStatus(isConnected: Boolean) {
+        val networkStatusOverlay = binding.networkStatusOverlay
+        val tvStatus = networkStatusOverlay.tvNetworkStatus
+        val ivStatusIcon = networkStatusOverlay.ivStatusIcon
+
+        if (isConnected) {
+            tvStatus.text = getString(R.string.connected)
+            tvStatus.setTextColor(ContextCompat.getColor(this, R.color.networkStatusIconTintOn))
+            ivStatusIcon.setImageResource(R.drawable.baseline_wifi_24)
+            ivStatusIcon.setColorFilter(
+                ContextCompat.getColor(
+                    this,
+                    R.color.networkStatusIconTintOn
+                )
+            )
+            showOverlay(networkStatusOverlay.root)
+            lifecycleScope.launch {
+                delay(2000)
+                hideOverlay(networkStatusOverlay.root)
+            }
+        } else {
+            tvStatus.text = getString(R.string.no_internet_connection)
+            tvStatus.setTextColor(ContextCompat.getColor(this, R.color.networkStatusIconTintOff))
+            ivStatusIcon.setImageResource(R.drawable.baseline_wifi_off_24)
+            ivStatusIcon.setColorFilter(
+                ContextCompat.getColor(
+                    this,
+                    R.color.networkStatusIconTintOff
+                )
+            )
+            showOverlay(networkStatusOverlay.root)
+        }
+    }
+
+    private fun showOverlay(view: View) {
+        view.visibility = View.VISIBLE
+        view.animate()
+            .translationY(0f)
+            .setDuration(500)
+            .start()
+    }
+
+    private fun hideOverlay(view: View) {
+        view.animate()
+            .translationY(-view.height.toFloat())
+            .setDuration(500)
+            .withEndAction { view.visibility = View.GONE }
+            .start()
+    }
+
+    // Extension to observe network status
+    private fun ConnectivityManager.observeNetworkStatus(): Flow<Boolean> = callbackFlow {
+        val callback = object : ConnectivityManager.NetworkCallback() {
+            override fun onAvailable(network: Network) {
+                trySend(true)
+            }
+
+            override fun onLost(network: Network) {
+                trySend(false)
+            }
+        }
+        registerDefaultNetworkCallback(callback)
+        awaitClose { unregisterNetworkCallback(callback) }
     }
 }
