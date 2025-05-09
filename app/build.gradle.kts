f8e13da730b481497037190ece210b30072dc695
iuturakulov <me@turakulov.ru>
2025-05-10 00:57:47 +0300

    MYBUDGET-45: delete unused resources, publish minor version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "ru.iuturakulov.mybudget"
         minSdk = 28
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
