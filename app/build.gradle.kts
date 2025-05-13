358c626d10017dc488856703ea5bdcc415624957
iuturakulov <me@turakulov.ru>
2025-05-13 04:54:55 +0300

    MYBUDGET-54: update app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         minSdk = 28
         targetSdk = 34
         versionCode = 3
-        versionName = "1.0.2"
+        versionName = "1.0.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
